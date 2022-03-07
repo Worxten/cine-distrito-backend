f932047b11a64355bb8fcdfa5db7de4bc2b123a3
KevAlex <kevin.alexdiaz@hotmail.com>
2022-03-07 18:58:16 -0500

    Controller added

--- src/main/java/com/udistrital/cinedistritobackend/api/controllers/user/UsuarioController.java
+++ src/main/java/com/udistrital/cinedistritobackend/api/controllers/user/UsuarioController.java
@@ -1,9 +1,11 @@
 package com.udistrital.cinedistritobackend.api.controllers.user;
 
+import com.udistrital.cinedistritobackend.api.controllers.payloads.UsuarioClientePayload;
 import com.udistrital.cinedistritobackend.api.services.user.usecase.RegistrarUsuarioUseCase;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/usuario")
@@ -15,6 +17,15 @@ public class UsuarioController {
         this.usuarioService = usuarioService;
     }
 
+    @GetMapping("/all")
+    public ResponseEntity getAllUsers(){
+        return new ResponseEntity(usuarioService.getAllUsers(), HttpStatus.OK);
+    }
 
+    @PostMapping("/nuevoUsuario")
+    public ResponseEntity createUser(@RequestBody UsuarioClientePayload payload){
+        return new ResponseEntity( (usuarioService.agregarUsuario(payload)),HttpStatus.OK );
+
+    }
 
 }
